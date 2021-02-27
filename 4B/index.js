8c806c1108d4e93a6511553472c8cacb9e958642
Ihsan <mn.ihsan09@gmail.com>
2021-02-27 21:15:01 +0700

    Tambah 4B

--- 4B/index.js
+++ 4B/index.js
@@ -44,7 +44,7 @@ app.post("/save", (req, res) => {
     stock: req.body.book_stock,
     image: req.body.book_image,
     deskripsi: req.body.book_deskripsi,
-    category_id: req.body.book_kategori,
+    category_id: req.body.book_category_id,
   };
   let sql = "INSERT INTO books SET ?";
   let query = connection.query(sql, data, (err, results) => {
@@ -53,7 +53,48 @@ app.post("/save", (req, res) => {
   });
 });
 
+// route untuk insert data kategori
+app.post("/save1", (req, res) => {
+  let data = {
+    nama: req.body.nama,
+  };
+  let sql = "INSERT INTO categories SET ?";
+  let query = connection.query(sql, data, (err, results) => {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
 // route untuk update
+app.post("/update", (req, ress) => {
+  let sql =
+    "UPDATE books SET name='" +
+    req.body.book_name +
+    "', stock='" +
+    req.body.book_stock +
+    "', image='" +
+    req.body.book_image +
+    "', deskripsi='" +
+    req.body.book_deskripsi +
+    "', category_id='" +
+    req.body.book_category_id +
+    "' WHERE id=" +
+    req.body.id;
+//   let sql = `UPDATE books SET name='${req.body.book_name}', stock='${req.body.book_stock}', image='${req.body.book_image}', deskripsi='${req.body.book_deskripsi}', category_id='${req.body.book_category_id}' WHERE id=${req.body.id}`;
+  let query = connection.query(sql, (err, results) => {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
+//route untuk delete data
+app.post('/delete',(req, res) => {
+    let sql = "DELETE FROM books WHERE id="+req.body.id+"";
+    let query = connection.query(sql, (err, results) => {
+      if(err) throw err;
+        res.redirect('/');
+    });
+  });
 
 app.listen(8000, () => {
   console.log("server running pro 8000");
--- db.sql
+++ db.sql
@@ -44,14 +44,6 @@ CREATE TABLE IF NOT EXISTS `categories` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=64 ;
 
---
--- Dumping data for table `user`
---
-
-INSERT INTO `categories` (`id`, `nama`, `alamat`, `pekerjaan`) VALUES
-(1, 'Andi', 'Surabaya', 'web programmer'),
-(2, 'Santoso', 'Jakarta', 'Web Designer'),
-(6, 'Samsul', 'Sumedang', 'Pegawai');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

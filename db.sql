ddcb8653fb82c30b5d9330b8e66af2660362d8aa
Ihsan <mn.ihsan09@gmail.com>
2021-02-27 21:29:14 +0700

    rubah sql

--- db.sql
+++ db.sql
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `books` (
 
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=64 ;
 
